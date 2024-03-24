a9c8da558ff19f537c606d9d5eb9768e97fda791
ofekbarel <143509831+ofekbarel@users.noreply.github.com>
2024-03-24 11:53:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/home')
-def sql_table():
-    return render_template('sq-data.html')
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
-if __name__ == '__main__':
+if __name__ == "__main__"":
     app.run(host="0.0.0.0")
-
-#rember the db is a container thtat runing on vm machine, so if the vm or the container are down,
-#the app will not working.
