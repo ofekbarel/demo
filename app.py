f960c9affef08b6bfc94fe8b506e79c51138db96
ofekbarel <143509831+ofekbarel@users.noreply.github.com>
2024-03-26 17:56:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def heelo_world():
-    return '<h1>Hello from my mytest</h2>'
+    return '<h1>pls work</h2>'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
